c70ad4fb826f86eaa1c48e061b01c079c31a0724
gfulton410 <gfulton@protonmail.com>
2018-04-27 23:24:42 -0400

    Fixed missing files

--- config/config.js
+++ config/config.js
@@ -1,14 +1,14 @@
 'use strict'
 
 // required environment variables
-[
+var arr = [ // Breaks when creating anonymous object...?
   'NODE_ENV',
   'PORT'
 ].forEach((name) => {
   if (!process.env[name]) {
     throw new Error(`Environment variable ${name} is missing`)
   }
-})
+});
 
 const config = {
   env: process.env.NODE_ENV,
